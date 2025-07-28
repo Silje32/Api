3ebfdf4c421079c475cb5c9d2c6fceec6d1b9dba
Silje32 <msh070425@gmail.com>
2025-07-28 10:33:19 +0200

    Started the project

---
+++ app.js
@@ -0,0 +1,18 @@
+const url = "http: //cyberpunk-edgerunners-api.vercel.app/char";
+
+const test = document.querySelector("#test");
+let CyberPunkChars = [];
+
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Could not fetch data");
+    }
+    return response.json();
+  })
+  .then((chars) => {
+    if ((chars = "")) {
+      throw new Error("array was empty");
+    }
+    CyberPunkChars = chars;
+  });
